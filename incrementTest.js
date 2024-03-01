bfe1d3688ca669593271dc9a18d5c33a11b09503
searsbd <searsbd@miamioh.edu>
2024-03-01 15:53:07 -0500

    added more spacing between the lines

--- incrementTest.js
+++ incrementTest.js
@@ -3,8 +3,12 @@
 // clicks is stored in local storage for the user, so the number of button
 // clicks is kept track of between sessions (even if the extension is closed).
 
+
+
 // Creates a variable that keeps track of the number of clicks (defaults to zero)
 let counter = 0;
+
+
 
 // This code tries to access the number of clicks from local storage. If there is no
 // stored number, no code runs and the default value of 0 for "counter" is kept. If there
@@ -15,9 +19,13 @@ chrome.storage.local.get(['counter'], function(result) {
     }
 });
 
+
+
 // Creates an event listener that runs the function increment() every time the
 // "Click to add to counter" button is clicked.
 document.getElementById('clickTracker').addEventListener('click', increment);
+
+
 
 // This function increases the counter by one, updates the paragraph tag that keeps track
 // of the total number of clicks on the main page, then finally updates value in of the counter
@@ -26,12 +34,15 @@ function increment() {
     // Increment the counter
     counter++;
 
+
+
     // Update the counter display
     document.getElementById("test").innerHTML = "Button has been clicked " + counter + " times.";
+
+
 
     // Save the new counter value to storage
     chrome.storage.local.set({counter: counter}, function() {
         console.log('Counter value is set to ' + counter);
     });
 }
-
--- listTest.js
+++ listTest.js
@@ -1,11 +1,17 @@
 // This is the JavaScript file that makes a to-do list.
 // There is still work to be done, as the list is not kept in storage yet.
 
+
+
 // Keeps track of number of tasks generated. It will be incremented by one
 // every time the "Add task" button is clicked. This is called "counter1" because,
 // incrementTest.js already has a variable named "counter" and I don't want there
 // to be any problems.
+
 let counter1 = 1;
+
+
+
 
 // This code removes the default text in the user input box on the main page.
 var textField1 = document.getElementById("description");
@@ -14,6 +20,9 @@ textField1.onfocus = function() {
         this.value = '';
     }
 }
+
+
+
 
 // This code runs every time the "Add task" button is clicked
 // It generates a new to do list item every time the button is clicked.
@@ -26,6 +35,8 @@ document.getElementById("listButton").addEventListener("click", function() {
     var value = textField.value;
     textField.value = "Add task here!";
 
+
+
     // Creates a new paragraph tag variable. Sets changes the value of the paragraph tag to what the user put
     // in the text field. Sets style.display to "inline-block" so the button and paragraph tag will
     // show up next to each other.
@@ -33,11 +44,15 @@ document.getElementById("listButton").addEventListener("click", function() {
     newElement.textContent = " " + value;
     newElement.style.display = "inline-block";
 
+
+
     // Creates a new button variable. The text that appears on the button says "Completed!". The style.display
     // to "inline-block" so the button and paragraph tag will show up next to each other.
     var newButton = document.createElement("button");
     newButton.textContent = "Completed!";
     newButton.style.display = "inline-block";
+
+
 
     // Adds an event listener to the newly generated button variable. When the button is clicked, it will delete
     // the itself and its corresponding paragraph tag. (In the code, parentNode describes the div that is created
@@ -47,18 +62,23 @@ document.getElementById("listButton").addEventListener("click", function() {
         newButton.parentNode.removeChild(newButton);
     });
 
+
+
     // Append the new paragraph and the new button to the list to a div. The button will be displayed first,
     // and the paragraph containing the task text will be shown next to it.
     var newDiv = document.createElement("div");
     newDiv.appendChild(newButton);
     newDiv.appendChild(newElement);
 
+
+
     // Creates a variable called list that is connected to the div on the index page with the id = "listTest"
     // We now add the div containing the button paragraph to list, that way it appears on the new web page.
     var list = document.getElementById("listTest");
     list.appendChild(newDiv);
 
+
+
     // Increments the total task counter
     counter1++;
 });
-
