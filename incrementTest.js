d4566789167648e6e446da5d3eb664ddaa710f82
searsbd <searsbd@miamioh.edu>
2024-03-01 15:46:28 -0500

    Made minor code changes and added tons of comments

--- incrementTest.js
+++ incrementTest.js
@@ -1,16 +1,27 @@
+// This is the JavaScript file that contains the code for the paragraph that
+// increases by one every time the button is clicked. The number of button
+// clicks is stored in local storage for the user, so the number of button
+// clicks is kept track of between sessions (even if the extension is closed).
 
+// Creates a variable that keeps track of the number of clicks (defaults to zero)
 let counter = 0;
-console.log("incremenet " + counter)
 
-// Try to load the counter value from storage
+// This code tries to access the number of clicks from local storage. If there is no
+// stored number, no code runs and the default value of 0 for "counter" is kept. If there
+// is a stored value, then the value of "counter" is change to that stored value.
 chrome.storage.local.get(['counter'], function(result) {
     if (result.counter !== undefined) {
         counter = result.counter;
     }
 });
 
-document.getElementById('click').addEventListener('click', increment);
+// Creates an event listener that runs the function increment() every time the
+// "Click to add to counter" button is clicked.
+document.getElementById('clickTracker').addEventListener('click', increment);
 
+// This function increases the counter by one, updates the paragraph tag that keeps track
+// of the total number of clicks on the main page, then finally updates value in of the counter
+// in local storage to the new value after the increase.
 function increment() {
     // Increment the counter
     counter++;
--- listTest.js
+++ listTest.js
@@ -1,49 +1,64 @@
+// This is the JavaScript file that makes a to-do list.
+// There is still work to be done, as the list is not kept in storage yet.
 
+// Keeps track of number of tasks generated. It will be incremented by one
+// every time the "Add task" button is clicked. This is called "counter1" because,
+// incrementTest.js already has a variable named "counter" and I don't want there
+// to be any problems.
 let counter1 = 1;
-console.log("list " + counter1)
 
-var textField = document.getElementById("description");
-textField.onfocus = function() {
+// This code removes the default text in the user input box on the main page.
+var textField1 = document.getElementById("description");
+textField1.onfocus = function() {
     if(this.value === 'Add task here!') {
         this.value = '';
     }
 }
 
+// This code runs every time the "Add task" button is clicked
+// It generates a new to do list item every time the button is clicked.
 document.getElementById("listButton").addEventListener("click", function() {
-var newElement = document.createElement("p");
-// Get the text field by its id
-var textField = document.getElementById("description");
+    // Creates a new text field object based on the contents of the index page text field.
+    // The value variable contains the text the user enters (or "Enter task here!" if they
+    // don't enter anything). Finally, it resets the value of the text field back to "Add task
+    // here!"
+    var textField = document.getElementById("description");
+    var value = textField.value;
+    textField.value = "Add task here!";
 
-// Get the value of the text field
-var value = textField.value;
+    // Creates a new paragraph tag variable. Sets changes the value of the paragraph tag to what the user put
+    // in the text field. Sets style.display to "inline-block" so the button and paragraph tag will
+    // show up next to each other.
+    var newElement = document.createElement("p");
+    newElement.textContent = " " + value;
+    newElement.style.display = "inline-block";
 
-// Set the value of the text field to blank
-textField.value = "Add task here!";
+    // Creates a new button variable. The text that appears on the button says "Completed!". The style.display
+    // to "inline-block" so the button and paragraph tag will show up next to each other.
+    var newButton = document.createElement("button");
+    newButton.textContent = "Completed!";
+    newButton.style.display = "inline-block";
 
-newElement.textContent = value;
-newElement.id = "element" + counter1;
-newElement.style.display = "inline-block";
-var newButton = document.createElement("button");
-newButton.textContent = "Completed!";
-newButton.id = "button" + counter1;
-newButton.style.display = "inline-block";
+    // Adds an event listener to the newly generated button variable. When the button is clicked, it will delete
+    // the itself and its corresponding paragraph tag. (In the code, parentNode describes the div that is created
+    // in the next code block)
+    newButton.addEventListener("click", function() {
+        newElement.parentNode.removeChild(newElement);
+        newButton.parentNode.removeChild(newButton);
+    });
 
+    // Append the new paragraph and the new button to the list to a div. The button will be displayed first,
+    // and the paragraph containing the task text will be shown next to it.
+    var newDiv = document.createElement("div");
+    newDiv.appendChild(newButton);
+    newDiv.appendChild(newElement);
 
-newButton.addEventListener("click", function() {
-    // Remove the corresponding paragraph and button elements
-    newElement.parentNode.removeChild(newElement);
-    newButton.parentNode.removeChild(newButton);
-});
+    // Creates a variable called list that is connected to the div on the index page with the id = "listTest"
+    // We now add the div containing the button paragraph to list, that way it appears on the new web page.
+    var list = document.getElementById("listTest");
+    list.appendChild(newDiv);
 
-// Append the new element and the new button to the list
-var newDiv = document.createElement("div");
-newDiv.appendChild(newButton);
-newDiv.appendChild(newElement);
-
-// Append the new div to the list
-var list = document.getElementById("listTest");
-list.appendChild(newDiv);
-
-counter1++;  // Increment counter
+    // Increments the total task counter
+    counter1++;
 });
 
