e261662500205badbc9d0c190cef566e4716fad1
ryanhorak21 <horakr@miamioh.edu>
2024-03-16 14:45:20 -0400

    Fullscreen & Exit fullscreen function test
    First attempt at creating a fullscreen button. Will remove if something is messed up.

---
+++ script.js
@@ -0,0 +1,21 @@
+const BUTTON = document.querySelector('button')
+const DIV = document.querySelector('div')
+
+// Toggle into and out of fullscreen function
+const toggleFullscreen = () => {
+    // Testing if screen is fullscreen or not
+    if (document.fullscreenElement)
+        // If true, we will exit fullscreen on button press
+        document.exitFullscreen()
+    else
+        // If false, we will enter fullscreen on button press
+        DIV.requestFullscreen()
+}
+
+BUTTON.addEventListener('click', toggleFullscreen)
+
+const onChange = () => {
+    DIV.className = document.fullscreenElement ? 'fullscreen' : ''
+}
+
+document.addEventListener('fullscreenchange', onChange)
