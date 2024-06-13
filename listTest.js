c883c37c6002b5cbc58f68c83d86c8416cfddc99
pegasiperspectives <113609176+pegasiperspectives@users.noreply.github.com>
2024-06-12 22:20:32 -0400

    Update listTest.js

--- listTest.js
+++ listTest.js
@@ -278,7 +278,7 @@ function createNewList() {
     numOfLists++; //increments the number of lists
 
     const numOfTabs = listArray.length; //sets this variable to the number of lists
-    const tabWidth = `${60 / numOfTabs}%`; // Sets tab width of the lists as a percentage based on the number of tabs 
+    const tabWidth = `${(100 / numOfTabs) * 0.80}%`; // Sets tab width of the lists as a percentage based on the number of tabs 
     const color = listColors[numOfLists] || "#C4A577"; //sets color for the list based on what's in storage or default color here
 
     const tabButton = document.createElement("button"); //creates a tab button for the list
